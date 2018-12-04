05a81c86dcba62434ae29d98c6f819b9bed56962
Chris Johnson <chris.johnson4@verizonwireless.com>
2018-12-04 11:21:00 -0500

    your commit message

---
+++ mix-list3/mix-list3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios', 'chrissyIos']
+print(list1)
+
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1[1])
+
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['juniper'])
+print(list1)
+
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['jene'])
+print(list1)
+
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['juniper'])
+print(list1)
+list1.append(['10.1.0.1', '10.2.0.1', '10.3.0.1'])
+print(list1)
+
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['juniper'])
+print(list1)
+list1.append(['10.1.0.1', '10.2.0.1', '10.3.0.1'])
+print(list1)
+print(list1[4]) 
+
+#!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['juniper'])
+print(list1)
+list1.append(['10.1.0.1', '10.2.0.1', '10.3.0.1'])
+print(list1)
+print(list1[4])
+print(list1[4][0])
+
+#!/usr/bin/env python3
+list1 = ['FOX', 'CAT', 'DOG', 'YAK', 'HOG']
+print(list1)
